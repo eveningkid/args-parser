bd35d552a6b73518bd47e1bb9f94e2f895176e8e
eveningkid <arn.dellinger@gmail.com>
2016-02-06 20:33:12 +0100

    Adding files to git

---
+++ parse.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/*
+	Straight-forward node.js arguments parser
+	Author: eveningkid
+	License: Apache-2.0
+*/
+
+function Parse (argv) {
+	// Removing node/bin and called script name
+	argv = argv.slice(2);
+
+	// Returned object
+	var args = {};
+
+	// For each argument
+	argv.forEach(function (arg, index) {
+		// Seperate argument, for a key/value return
+		arg = arg.split('=');
+
+		// Retrieve the argument name
+		var argName = arg[0];
+		
+		// Remove the two "--" in front of the argument
+		if (argName.slice(0, 2) == "--") {
+			argName = argName.slice(2);
+		}
+
+		// Or if it is a single hyphen
+		if (argName.slice(0, 1) == '-') {
+			argName = argName.slice(1);
+		}
+
+		// Associate defined value or initialize it to "undefined" state
+		var argValue = (arg.length === 2) ? arg[1] : undefined;
+
+		// Finally add the argument to the args set
+		args[argName] = argValue;
+	});
+
+	return args;
+}
+
+module.exports = Parse;
