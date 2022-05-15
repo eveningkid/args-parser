51246fcfdde6a0e4ab827da777cdf657eedaafd5
vipul.vyas <vipulvyas0813@gmail.com>
2022-05-15 22:16:33 +0530

    check for if argument fail on regex then give error message, but not breaking their application

--- parse.js
+++ parse.js
@@ -8,16 +8,20 @@
 
 const ARGUMENT_SEPARATION_REGEX = /([^=\s]+)=?\s*(.*)/;
 
-function Parse(argv) {
+const Parse = (argv) => {
   // Removing node/bin and called script name
   argv = argv.slice(2);
 
   const parsedArgs = {};
   let argName, argValue;
 
-  argv.forEach(function (arg) {
+  for (let arg of argv) {
     // Separate argument for a key/value return
     arg = arg.match(ARGUMENT_SEPARATION_REGEX);
+    if (!arg) {
+      console.error("error: invalid syntax!!\nsyntax: node script.js careful -dangerous --tomatoes=3 --tonight --key=ek==\nfor more visit https://www.npmjs.com/package/args-parser");
+      return {};
+    }
     arg.splice(0, 1);
 
     // Retrieve the argument name
@@ -37,7 +41,7 @@ function Parse(argv) {
         : true;
 
     parsedArgs[argName] = argValue;
-  });
+  };
 
   return parsedArgs;
 }
